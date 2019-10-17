2dca81071a39027d660025de124609bbc1d0e43e
kinyunye <kamaugerald2@gmail.com>
2019-10-17 14:14:39 +0300

    List View

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,34 @@ import 'package:primo/screens/home.dart';
 
 void main() => runApp(MaterialApp(title: "Exploring UI widgets",
   debugShowCheckedModeBanner: false,
-  home: Home(),));
+  home: Scaffold(
+    appBar: AppBar(title: Text("List View"),),
+    body: getListView(),
+  ),));
 
+Widget getListView() {
+  var listView = ListView(
+    children: <Widget>[
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View!"),
+        trailing: Icon(Icons.wb_sunny),
+      ),
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View!"),
+        trailing: Icon(Icons.wb_sunny),
+      ),
+      ListTile(
+        leading: Icon(Icons.landscape),
+        title: Text("Landscape"),
+        subtitle: Text("Beautiful View!"),
+        trailing: Icon(Icons.wb_sunny),
+      ),
 
-
+    ],
+  );
+  return listView;
+}
